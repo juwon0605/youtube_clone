e3d7d66ac429c8e66c26b323fd17332a758291f8
juwon0605 <58173061+juwon0605@users.noreply.github.com>
2020-12-04 02:51:34 +0900

    routes with express

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hi!");
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+    console.log(`Listening on: http://localhost: ${PORT}`);
+}
+
+const handleHome = (req, res) => {
+    console.log(req);
+    res.send("Hello frome home!");
+}
+const handleProfjile = (req, res) => {
+    res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+app.get("/profile", handleProfjile);
+
+app.listen(PORT, handleListening);
