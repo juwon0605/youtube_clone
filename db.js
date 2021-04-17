3bec475b1bf32f70bcd0d00fc35b66782e2bf3e5
juwon <58173061+juwon0605@users.noreply.github.com>
2021-04-18 02:35:41 +0900

    set mongoDB using env

--- db.js
+++ db.js
@@ -1,50 +1,19 @@
-export const videos = [
-    {
-        id:1,
-        title: `Video1`,
-        description: `This is something I like`,
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 111111,
-            name: "Juwon",
-            email: "Juwon@example.com"
-        }
-    },
-    {
-        id:2,
-        title: `Video2`,
-        description: `This is something I like`,
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 222222,
-            name: "Juwon",
-            email: "Juwon@example.com"
-        }
-    },
-    {
-        id:3,
-        title: `Video3`,
-        description: `This is something I like`,
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 333333,
-            name: "Juwon",
-            email: "Juwon@example.com"
-        }
-    },
-    {
-        id:4,
-        title: `Video4`,
-        description: `This is something I like`,
-        views: 24,
-        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 444444,
-            name: "Juwon",
-            email: "Juwon@example.com"
-        }
-    }
-]
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = error => console.log(`Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
